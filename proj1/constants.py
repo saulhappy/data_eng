76d1ef824f316bfae79314d272c092eca3c96b6b
saulhappy <646634@gmail.com>
2021-10-13 13:32:48 -0500

    successful request completed

--- proj1/constants.py
+++ proj1/constants.py
@@ -1,1 +1,2 @@
 URL = "https://dark-sky.p.rapidapi.com/30.360090,-97.692980"
+HOST = "dark-sky.p.rapidapi.com"
--- proj1/main.py
+++ proj1/main.py
@@ -1,4 +1,23 @@
-from creds.keys import KEYS
+import pandas as pd
+import requests
+import json
 
-print(KEYS["x-rapidapi-key"])
+from creds.keys import KEYS
+from constants import URL, HOST
+
+
+url = URL
+
+querystring = {"units":"auto","lang":"en"}
+
+headers = {
+    'x-rapidapi-host': HOST,
+    'x-rapidapi-key': KEYS["x-rapidapi-key"]
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+
+
 
