586ac9cad2ebf99588b2d24ee8c9aaee7c53ee4b
saulhappy <646634@gmail.com>
2021-10-13 17:38:25 -0500

    extracted interested data

--- proj1/main.py
+++ proj1/main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import requests
 import json
+import os
 
 from creds.keys import KEYS
 from constants import URL, HOST
 
+os.system('cls' if os.name == 'nt' else 'clear')
 
 url = URL
 
@@ -16,8 +18,17 @@ headers = {
 }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
+data = response.json()
 
-print(response.text)
+summary = data["daily"]["summary"]
+time = data["daily"]["data"][0]["time"]
+sunrise_time = data["daily"]["data"][0]["sunriseTime"]
+sunset_time = data["daily"]["data"][0]["sunsetTime"]
+temp_high = data["daily"]["data"][0]["temperatureHigh"]
+temp_low = data["daily"]["data"][0]["temperatureLow"]
+humidity = data["daily"]["data"][0]["humidity"]
+
+print(summary, time, sunrise_time, sunrise_time, temp_high, temp_low, humidity)
 
 
 
