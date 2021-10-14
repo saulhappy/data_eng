eb0d658dfcc11fd6a74cd3929fb6c208c0c7ebd8
saulhappy <646634@gmail.com>
2021-10-14 17:09:54 -0500

    added format data method with unit test

--- proj1/TestWeatherData.py
+++ proj1/TestWeatherData.py
@@ -6,6 +6,7 @@ from WeatherData import WeatherData
 from constants import URL, HEADERS, QUERY_STRING
 
 class TestWeatherData(unittest.TestCase):
+    response = requests.request("GET", URL, headers=HEADERS, params=QUERY_STRING)
 
     def setUp(self):
         self.weather_data = WeatherData()
@@ -14,9 +15,21 @@ class TestWeatherData(unittest.TestCase):
          self.assertIsInstance(self.weather_data, WeatherData)
     
     def test_query_api(self):
-        response = requests.request("GET", URL, headers=HEADERS, params=QUERY_STRING)
-        data = response.json()
+        data = self.response.json()
         self.assertIn("daily", data)
+
+    def test_get_formatted_data(self):
+        wd = WeatherData()
+        wd.format_data()
+        data = wd.get_formatted_data()
+        self.assertIn("summary", data)
+        self.assertIn("time", data)
+        self.assertIn("sunrise_time", data)
+        self.assertIn("sunset_time", data)
+        self.assertIn("temp_high", data)
+        self.assertIn("temp_low", data)
+        self.assertIn("humidity", data)
+
 
 
 if __name__ == "__main__":
--- proj1/WeatherData.py
+++ proj1/WeatherData.py
@@ -3,19 +3,43 @@ import requests
 import json
 import os
 
-from collections import defaultdict
 from constants import URL, HEADERS, QUERY_STRING
 
 class WeatherData():
     def __init__(self):
-        self.data = defaultdict
+        self.data = {}
 
     def query_api(self):
         response = requests.request("GET", URL, headers=HEADERS, params=QUERY_STRING)
         return response.json()
 
     def format_data(self):
-        pass
+        data = self.query_api()
+
+        summary = data["daily"]["summary"]
+        time = data["daily"]["data"][0]["time"]
+        sunrise_time = data["daily"]["data"][0]["sunriseTime"]
+        sunset_time = data["daily"]["data"][0]["sunsetTime"]
+        temp_high = data["daily"]["data"][0]["temperatureHigh"]
+        temp_low = data["daily"]["data"][0]["temperatureLow"]
+        humidity = data["daily"]["data"][0]["humidity"]
+
+        formated_data = {
+            "summary": summary,
+            "time" : time,
+            "sunrise_time": sunrise_time,
+            "sunset_time": sunset_time,
+            "temp_high": temp_high,
+            "temp_low": temp_low,
+            "humidity": humidity
+        }
+        self.data = formated_data
+
+    def get_formatted_data(self):
+        return self.data
+    
+
+
 
 
 
